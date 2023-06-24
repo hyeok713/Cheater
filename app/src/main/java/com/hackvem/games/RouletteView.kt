203663939b401bedea6355ddb27094dfe9b701c4
HYEOKBEOM <part1shb@gmail.com>
2023-06-24 17:52:50 +0900

    feat: icon added

--- app/src/main/java/com/hackvem/games/RouletteView.kt
+++ app/src/main/java/com/hackvem/games/RouletteView.kt
@@ -2,36 +2,50 @@ package com.hackvem.games
 
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.lang.Math.cos
+import java.lang.Math.sin
 
 @Composable
-fun RouletteWheel() {
-    Box(
+fun RouletteWheel(
+    itemList: List<String> = listOf(
+        "hackvem",
+        "hackve",
+        "hackvem",
+        "hackvem",
+    ),
+) {
+    var isStarted by remember { mutableStateOf(false) }
+    BoxWithConstraints(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
+
         val infiniteTransition = rememberInfiniteTransition()
 
-        val rotation by infiniteTransition.animateFloat(
+        val rotation: Float by infiniteTransition.animateFloat(
             initialValue = 0f,
-            targetValue = 3060f,    // 타겟을 설정 해야함
+            targetValue = 3060f,    // set targetValue randomly
             animationSpec = infiniteRepeatable(
                 animation = tween<Float>(
                     durationMillis = 5000,
@@ -40,36 +54,80 @@ fun RouletteWheel() {
             )
         )
 
+        val animatedProgress = animateFloatAsState(
+            targetValue = 3060f,
+            animationSpec = repeatable(
+                iterations = 2,
+                animation = tween<Float>(
+                    durationMillis = 5000,
+                    easing = FastOutSlowInEasing,
+                ),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
+
+        val colors = COLOR_LIST.shuffled().subList(0, itemList.size)
+
         Canvas(modifier = Modifier.fillMaxSize(0.9f)) {
-            rotate(rotation) {
-                drawRouletteWheel(8)
+            rotate(animatedProgress.value) {
+                drawRouletteWheel(
+                    itemList,
+                    colors
+                )
+            }
+        }
+
+        Icon(
+            painter = painterResource(id = R.drawable.ic_arrow),
+            contentDescription = "Arrow Icon",
+            modifier = Modifier
+                .size(48.dp)
+                .offset(0.dp, -(maxWidth / 2))
+        )
+
+        if (!isStarted) {
+            IconButton(
+                onClick = { isStarted = true },
+                modifier = Modifier.size(128.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_start),
+                    contentDescription = "시작버튼",
+                )
             }
         }
     }
 }
 
-
 private const val ROUND_ANGLE = 360f
-private fun DrawScope.drawRouletteWheel(capability: Int = 3) {
+
+private val COLOR_LIST = listOf(
+    Color.Red.copy(blue = 0.1f, green = 0.2f),
+    Color.Magenta,
+    Color.Yellow,
+    Color.Green,
+    Color.Blue.copy(green = 0.5f),
+    Color.Cyan,
+    Color.Yellow.copy(red = 0.1f, blue = 0.4f, green = 0.6f),
+    Color.Red.copy(red = 0.6f, blue = 0.3f)
+)
+
+/**
+ * drawRouletteWheel
+ * @param itemList List<String>
+ * - requires string item list to put text each surface of parts
+ */
+private fun DrawScope.drawRouletteWheel(
+    itemList: List<String>,
+    colors: List<Color>,
+) {
     val wheelRadius = size.minDimension / 2
     val centerX = size.width / 2
     val centerY = size.height / 2
-    val strokeWidth = 4.dp.toPx()
+    val strokeWidth = 7.dp.toPx()
 
-    val angle = ROUND_ANGLE / capability
-
-    val colorList = listOf(
-        Color.Red,
-        Color.Magenta,
-        Color.Yellow,
-        Color.Green,
-        Color.Blue,
-        Color.Cyan,
-        Color.LightGray,
-        Color.White
-    ).map { it.copy(alpha = 0.9f) }
-
-    val texts = listOf("A","B","C","D","E","F","G","H")
+    val angle = ROUND_ANGLE / itemList.size
 
     // Draw the outer circle
     drawCircle(
@@ -79,20 +137,21 @@ private fun DrawScope.drawRouletteWheel(capability: Int = 3) {
         style = Stroke(strokeWidth)
     )
 
-    // Draw text on each half
-    val textSize = 24.sp.toPx()
-    val textOffset = 0.4f // Offset to position the text within the halves
+    // Draw arcs and text on each split part
+    val textSize = (if (itemList.any { it.length >= 6 }) 16 else 20).sp.toPx()
     val textPaint = android.graphics.Paint().apply {
         color = android.graphics.Color.BLACK
         textAlign = android.graphics.Paint.Align.CENTER
         this.textSize = textSize
     }
 
-    repeat(capability) {
-        // Draw the first half of the circle
+    repeat(itemList.size) { index ->
+        val startAngle = angle * index
+
+        // Draw the split part
         drawArc(
-            color = colorList[it],
-            startAngle = angle * it,
+            color = colors[index],
+            startAngle = startAngle,
             sweepAngle = angle,
             useCenter = true,
             topLeft = Offset(centerX - wheelRadius, centerY - wheelRadius),
@@ -100,10 +159,18 @@ private fun DrawScope.drawRouletteWheel(capability: Int = 3) {
             style = Fill
         )
 
+        // Calculate the position of the text
+        val textAngle = startAngle + angle / 2
+        val textX =
+            centerX + (wheelRadius / 1.5f - textSize / 2) * cos(Math.toRadians(textAngle.toDouble())).toFloat()
+        val textY =
+            centerY + (wheelRadius / 1.5f - textSize / 2) * sin(Math.toRadians(textAngle.toDouble())).toFloat()
+
+        // Draw the text
         drawContext.canvas.nativeCanvas.drawText(
-            texts[it],
-            centerX,
-            centerY,
+            itemList[index],
+            textX,
+            textY,
             textPaint
         )
     }
